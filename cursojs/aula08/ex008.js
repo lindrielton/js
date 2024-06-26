3a650a267b6b0dcf0db080935c72b277c1431eb7
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2024-06-25 23:00:48 -0300

    desafio completo

---
+++ cursojs/aula08/ex008.js
@@ -0,0 +1,5 @@
+var vel = 60.5
+console.log(`a velocidade do seu carro é ${vel} km/h`)
+if (vel > 65){
+    console.log(`a velocidade do seu carro ultrapassou o limite permitido, vc vai ser multado!`)
+}
---
+++ cursojs/aula08/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'mexico'
+console.log(`vivendo em ${pais}`)
+if (pais == 'brasil'){
+    console.log('vc é brasileiro')
+} else {
+    console.log('vc é estrangeiro')
+}
