c189a411b5365ce209ae31eeb3e697299877ab16
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2024-06-30 22:08:17 -0300

    primeiro ex

---
+++ cursojs/aula12ex/script/script.js
@@ -0,0 +1,22 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var tempo = window.document.getElementById('imagens')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `a hora atual é ${hora} horas.`
+    
+    if (hora >= 0 &&  hora < 12){
+        msg.innerHTML = 'bom dia com jesus'
+        tempo.src = 'imagens/bomdia.png'
+        document.body.style.background = '#FAD39E'
+
+    }else if ( hora > 12 && hora <= 18){
+        msg.innerHTML = 'boa tarde com maria'
+        tempo.src = 'imagens/boatarde.png'
+        document.body.style.background = '#F7A716'
+    }else {
+        msg.innerHTML = 'boa noite com Deus'
+        tempo.src = 'imagens/boanoite.png'
+        document.body.style.background = '#2D3347'
+    }
+}
